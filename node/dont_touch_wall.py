d4e6de961bc16459fec428359d93ed15709fe43b
nulha <nulha01@naver.com>
2024-04-17 01:03:58 +0900

    first commit

--- node/dont_touch_wall.py
+++ node/dont_touch_wall.py
@@ -11,19 +11,25 @@ pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
 
 rate = rospy.Rate(5)
 
+angular_speed = None
 
-min = -2
-max = 2
+def update_angular_speed():
+    new_value = random.uniform(3, 3.5)
+    return new_value if random.choice([True, False]) else -new_value
 
 
 def sub_callback(pose):
+    global angular_speed 
     go = Twist()
-    if pose.x < 0.8 or pose.x > 10.2 or pose.y< 0.8 or pose.y > 10.2 :
-        go.linear.x = 1
-        go.angular.z = 2
+    if pose.x < 1 or pose.x > 10 or pose.y< 1 or pose.y > 10 :
+        go.linear.x = 1.8
+        if angular_speed is None:
+            angular_speed = update_angular_speed()
+        go.angular.z = angular_speed
     else :
         go.linear.x = random.uniform(1,3)
-        go.angular.z = random.uniform(min,max)
+        go.angular.z = random.uniform(-3,3)
+        angular_speed = None
     pub.publish(go)
 
 
